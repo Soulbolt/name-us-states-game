40861ce7ef3ed34b34ec68ba499ef6a424417653
Soulbolt <soulbolt@gmail.com>
2023-12-17 22:47:23 -0700

    Added code setting up screen, turtle, and csv data access. Created prompt to ask user to enter name of state, checks if is on the list, if it is then it displays state's name on the map accordingly. Shows score of how many states have been named and keeps track of names already found. Game ends when all 50 states have been named.(installed and use pandas to get csv data.)

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+# get image to use as a new shape(background)
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+state_name = turtle.Turtle()
+state_name.penup()
+state_name.hideturtle()
+
+# Reading csv data with Pandas
+data = pandas.read_csv("50_states.csv")
+# May need to convert to list or dictionary to iterate and match user input.
+state_list = data.state.to_list()
+guessed_list = []
+
+title = "Guess the State"
+while len(guessed_list) < 50:
+    user_answer = screen.textinput(title=f"{len(guessed_list)}/50 States Correct", prompt="What's another state's name?").title()
+    states = data[data.state == user_answer]
+    x_cor = list(states.x)
+    y_cor = list(states.y)
+
+    if user_answer in guessed_list:
+        print(f"{user_answer} already listed")
+    elif user_answer in state_list:
+        guessed_list.append(user_answer)
+        state_name.goto(x_cor[0], y_cor[0])
+        state_name.write(user_answer)
+    else:
+        print(f"{user_answer} does not exist.")
+
+screen.exitonclick()
+# def get_mouse_click_cor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_cor)
+#
+# turtle.mainloop()
